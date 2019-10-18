acf6ac81d8c0c9540a022132126d1841cf8091a7
Dominic Leung <leungcheuk209@hotmail.com>
2019-10-19 00:13:26 +0800

    Backtest 2nd ccleaned

--- backtest.py
+++ backtest.py
@@ -12,63 +12,73 @@ f = h5.File("/Users/dominicleung/OneDrive/Documents/FINA4390/stockprice/HKSTOCK3
 label = pd.read_csv('/Users/dominicleung/OneDrive/Documents/FINA4390/industry/PE_NDE.csv')
 
 ###---------------------------------------------------------
-### Calculate and merge return data
+### Predefined function
 ###---------------------------------------------------------
 def resample(group):
     return group.resample('M').last()
 
-item_list = list(f.keys())
-#Input data from hdf5
-new = True
-for item in item_list:
-    stock = np.array(f[item])
-    stock_df = pd.DataFrame(stock)
-    if new:
-        df = pd.DataFrame(columns = stock_df.columns)
-        new = False
-    df = df.append(stock_df.copy(deep = True), sort = False)
+def bt_df(f, label):
+    """ Return the dataframe that matches holding return with prediction """
+    item_list = list(f.keys())
+    #Input data from hdf5
+    new = True
+    count = 0
+    for item in item_list:
+        stock = np.array(f[item])
+        stock_df = pd.DataFrame(stock)
+        if new:
+            df = pd.DataFrame(columns = stock_df.columns)
+            new = False
+        df = df.append(stock_df.copy(deep = True), sort = False)
+        count +=1
+        if count % 100 ==0:
+            print(count)
 
-#Resample data to monthly
-df['Date'] = pd.to_datetime(df['Date'].str.decode("utf-8"))
-df['Ticker'] = df['Ticker'].str.decode("utf-8")
-df.set_index('Date', inplace = True)
-result = df.groupby('Ticker').apply(resample)
-result.drop(['Ticker'], axis = 1, inplace = True) #Duplicate columns that should be removed
-bt = pd.DataFrame(result.groupby('Ticker')['Nominal Price'].pct_change().shift(-1)) #Make as the return if we long at that time
-bt.rename(columns = {"Nominal Price": "Monthly Return"},  inplace = True)
-bt.reset_index(inplace = True)
-bt.set_index(['Ticker','Date'], inplace = True)
+    #Resample data to monthly
+    df['Date'] = pd.to_datetime(df['Date'].str.decode("utf-8"))
+    df['Ticker'] = df['Ticker'].str.decode("utf-8")
+    df.set_index('Date', inplace = True)
+    result = df.groupby('Ticker').apply(resample)
+    result.drop(['Ticker'], axis = 1, inplace = True) #Duplicate columns that should be removed
+    bt = pd.DataFrame(result.groupby('Ticker')['Nominal Price'].pct_change().shift(-1)) #Make as the return if we long at that time
+    bt.rename(columns = {"Nominal Price": "Monthly Return"},  inplace = True)
+    bt.reset_index(inplace = True)
+    bt.set_index(['Ticker','Date'], inplace = True)
 
-#Modify prediction file 
-label['Date'] = pd.to_datetime(label['Date'], format = "%Y%m%d")
-for i in range(len(label)): #Change label to match with naming convention of QUANDL
-    ticker = label.iloc[i,1][:-10]
-    if len(ticker)<5:
-        for j in range(len(ticker),5):
-            ticker = '0'+ticker
-    ticker = 'HKEX/'+ticker
-    label.iloc[i,1] = ticker
-label = label.set_index(['Date']).groupby('Ticker').resample('M').ffill()
-label.drop('Ticker', axis = 1, inplace = True)
+    #Modify prediction file
+    label['Date'] = pd.to_datetime(label['Date'], format = "%Y%m%d")
+    for i in range(len(label)): #Change label to match with naming convention of QUANDL
+        ticker = label.iloc[i,1][:-10]
+        if len(ticker)<5:
+            for j in range(len(ticker),5):
+                ticker = '0'+ticker
+        ticker = 'HKEX/'+ticker
+        label.iloc[i,1] = ticker
+    label = label.set_index(['Date']).groupby('Ticker').resample('M').ffill()
+    label.drop('Ticker', axis = 1, inplace = True)
+    backtest = label.merge(bt, left_on = ['Ticker', 'Date'], right_on = ['Ticker', 'Date']).reset_index()
 
+    return backtest
 
 ###---------------------------------------------------------
 ### THe final result
 ### Analysis starts at here
 ###---------------------------------------------------------
-backtest = label.merge(bt, left_on = ['Ticker', 'Date'], right_on = ['Ticker', 'Date'])
-backtest.reset_index(inplace = True)
-grp = pd.DataFrame()
-for i in set(list(label.pred)):
-    grp[i] = backtest.loc[backtest['pred']==i].groupby('Date')['Monthly Return'].mean()
+if __name__ == "__main__":
+    backtest = bt_df(f, label)
+    grp = pd.DataFrame()
+    for i in set(list(label.pred)):
+        grp[i] = backtest.loc[backtest['pred']==i].groupby('Date')['Monthly Return'].mean()
 
-#Plot the performance for each group
-plt.rcParams["figure.figsize"] = [10,5]
-grp.loc[:,[3,5]].plot()
+    #Plot the performance for each group
+    plt.rcParams["figure.figsize"] = [10,5]
+    grp.plot()
+    plt.show()
 
-#Count number of stocks for each data
-grpcount = pd.DataFrame(backtest.groupby(['Date','pred'])['Ticker'].count())
-grpcount.groupby('pred')['Ticker'].plot()
+    # #Count number of stocks for each data
+    grpcount = pd.DataFrame(backtest.groupby(['Date','pred'])['Ticker'].count())
+    grpcount.groupby('pred')['Ticker'].plot()
+    plt.show()
 
 
 
