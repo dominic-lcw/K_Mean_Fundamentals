f3e2048c9253af5de9325434d951d32b981ded9a
Dominic Leung <leungcheuk209@hotmail.com>
2019-11-05 16:32:25 +0800

    Scatterplot Function

--- K_Mean.R
+++ K_Mean.R
@@ -1,7 +1,7 @@
 ###----------------------------------------
 ### Input Raw Data
 ###----------------------------------------
-source <- "/Users/dominicleung/Documents/4390Local/Business_Financial_Risk.csv"
+source <- "/Users/dominicleung/Documents/4390Local/Raw_Data/Business_Financial_Risk.csv"
 raw <- read.csv(source)
 col_len = dim(raw)[2]
 
@@ -18,15 +18,15 @@ for (i in 3:col_len){
 ###----------------------------------------
 ### Input Cleaned Data
 ###----------------------------------------
-source <- "/Users/dominicleung/Documents/4390Local/Market_Related/Train/mr6.csv"
+source <- "/Users/dominicleung/Documents/4390Local/Market_Related/Training/mr1.csv"
 d <- read.csv(source)
 head(d)
 ###----------------------------------------
 ### Box plot for all columns
 ###----------------------------------------
-name = colnames(d)
+name = colnames(d);name
 col_len = dim(d)[2]
-row_len = dim(d)[1]
+row_len = dim(d)[1];row_len
 
 par(mfrow = c(1,4))
 for (i in 3:col_len){
@@ -36,34 +36,54 @@ for (i in 3:col_len){
 ###----------------------------------------
 ### Train the best KM model
 ###----------------------------------------
-bestk <- function(d1, min){
-  for (i in 3:10){
-    set.seed(8)
-    km <- kmeans(d1, i, nstart = 50)
-    if (sort(table(km$cluster), decreasing = TRUE)[2]>min){
-      km.out <- km; cat("K=",i)
-      break
+bestk <- function(d1){
+  n = dim(d1)[1]
+  min = floor(n/3)
+  while(min > 200){
+    for (i in 3:20){
+      set.seed(8)
+      km <- kmeans(d1, i, nstart = 50, iter.max = 100, algorithm="MacQueen")
+      if (sort(table(km$cluster), decreasing = TRUE)[2]>min){
+        km.out <- km; cat("K=",i)
+        table(km.out$cluster) #Evaluate Distribution
+        return(km.out)
+      }
     }
+    min = min - 100
   }
-  km.out
+  cat("FAILED")
+  return(NULL)
 }
-head(d1)
-d1 = d[3:col_len]
 
-km.r <- bestk(d1,200)
-plot(d1, col = km.r$cluster)
-table(km.r$cluster) #Evaluate Distribution
+scattermatrix <- function(km.r, df){
+  group = as.numeric(names(sort(table(km.r$cluster), decreasing = TRUE)))
+  max_index = group[1:4]
+
+  largest = cbind(df, km.r$cluster)
+  in_set = subset(largest, (km.r$cluster %in% max_index))
+  out_set = subset(largest, !(km.r$cluster %in% max_index))
+  out_set['km.r$cluster'] = 'black'
+
+  colour = c('red', 'blue','green','lightblue','purple', 'orange')  
+  for (i in 1:length(max_index)){
+    in_set[in_set['km.r$cluster']== max_index[i],]['km.r$cluster'] = colour[i]
+  }
+  total_set = rbind(out_set, in_set)
+  n = as.numeric(dim(total_set)[2])
+  plot(total_set[1:(n-1)], col = total_set[,n])
+}
+
+scattermatrix(km.r, d1)
 
 ###----------------------------------------
 ### Decide multiple columns that are representable
 ###----------------------------------------
-sel = c(1,3,4,6)
+sel = c(1,2,4)
 selected = d1[sel]
 x1 <- data.frame(selected)
 par(mfrow = c(1,1))
 plot(x1, col = km.r$cluster, main = "before")
 legend(x = 1.5,y = 2, legend = c('1','2','3','4','5','6','7','8','9'),lwd = 1, col = c(1:9), cex = 0.4)
-
 
 library("nnet")
 p0 <- data.frame(selected, km.r$cluster) 
@@ -80,16 +100,16 @@ table(pred) #Evaluate transformed result
 output <- data.frame(d[1], d[2], selected, pred)
 #output <- data.frame(d[1], d[2], selected, km.r$cluster)
 head(output)
-write.csv(output, file = "/Users/dominicleung/Documents/4390Local/Market_Related/mr6_pred.csv", row.names = FALSE)
+write.csv(output, file = "/Users/dominicleung/Documents/4390Local/Operating_Profitability/op2_pred.csv", row.names = FALSE)
 
 ###----------------------------------------
 ### Validation Dataset Result
 ###----------------------------------------
-source <- "/Users/dominicleung/Documents/4390Local/Market_Related/Validation/mr6.csv"
+source <- "/Users/dominicleung/Documents/4390Local/Operating_Profitability/Validation/op2.csv"
 val <- read.csv(source)
 len <-dim(val)[2]
 v1 <- val[c(3:len)][sel]
 v_pred <-predict(mn1, v1)
 table(v_pred)
 output2 <- data.frame(val[1], val[2], v1, v_pred)
-write.csv(output2, file = "/Users/dominicleung/Documents/4390Local/Market_Related/mr6_val.csv", row.names = FALSE)
+write.csv(output2, file = "/Users/dominicleung/Documents/4390Local/Operating_Profitability/op2_val.csv", row.names = FALSE)
